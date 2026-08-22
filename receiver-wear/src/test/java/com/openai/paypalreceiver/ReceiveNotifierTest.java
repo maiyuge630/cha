09d46f6ef066573ef4a73584eba366bc7e0339b9
maiyuge630 <maiyuge630@gmail.com>
2026-08-22 15:02:59 -0700

    Test BOA withdrawal call-to-action in receive alert

--- receiver-wear/src/test/java/com/openai/paypalreceiver/ReceiveNotifierTest.java
+++ receiver-wear/src/test/java/com/openai/paypalreceiver/ReceiveNotifierTest.java
@@ -6,14 +6,20 @@ import org.junit.Test;
 
 public class ReceiveNotifierTest {
     @Test
-    public void paypalTitleUsesMessageOnly() {
-        assertEquals("Alex sent you $25.00",
+    public void paypalTitleUsesMessageAndBoaAction() {
+        assertEquals("Alex sent you $25.00\n点此提现到 BOA",
                 ReceiveNotifier.composeContent("PayPal", "Alex sent you $25.00"));
     }
 
     @Test
-    public void senderTitleIsPreserved() {
-        assertEquals("Alex · sent you $25.00",
+    public void senderTitleIsPreservedWithBoaAction() {
+        assertEquals("Alex · sent you $25.00\n点此提现到 BOA",
                 ReceiveNotifier.composeContent("Alex", "sent you $25.00"));
+    }
+
+    @Test
+    public void emptyNotificationStillHasUsefulAction() {
+        assertEquals("收到一笔 PayPal 款项\n点此提现到 BOA",
+                ReceiveNotifier.composeContent("", ""));
     }
 }
